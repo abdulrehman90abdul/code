e890175ab1bd974a69dd05e3f67f43c3d5a9e7da
abdulrehman90abdul <abdulrehman90abdul90>
2024-01-16 18:37:22 +0300

    31

--- code2/script.py
+++ code2/script.py
@@ -1,3 +1,14 @@
 print("hello world")
 print("hello world")
 print("hello world")
+
+import random
+print(random.randint(3,10))
+print(random.randint(3,10))
+print(random.randint(3,10))
+print(random.randint(3,10))
+print(random.randint(3,10))
+print(random.randint(3,10))
+print(random.randint(3,10))
+print(random.randint(3,10))
+print(random.randint(3,10))
